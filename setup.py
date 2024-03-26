1ff786206373e4a0bd93b16cab63d2ceb722f4d8
ujj-kumar <batman.c731@gmail.com>
2024-03-26 13:50:26 +0530

    Changing User Name and removing venv folder

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='MLProject', 
     version='0.0.1',
-    author="Ujjwal",
+    author="Ujjwal Kumar",
     author_email='batman.c731@gmail.com',
     packages=find_packages(), 
     install_requires=get_requirements('requirements.txt')
